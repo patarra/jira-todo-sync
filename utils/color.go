0286f41f799b751bf533b06970c45d4cb58ef34f
Jose Manuel Felguera <jfelguerarodriguez@twilio.com>
2021-05-07 12:33:34 +0200

    Fixed utils to not print [] for every parameter

--- utils/color.go
+++ utils/color.go
@@ -8,20 +8,26 @@ import (
 var blue = color.New(color.Bold,color.FgBlue)
 var yellow = color.New(color.Bold,color.FgYellow)
 var red = color.New(color.Bold,color.FgRed)
+var green = color.New(color.Bold, color.FgGreen)
+
+func PrintSuccessF(format string, a ...interface{}){
+	green.Print("[OK] ")
+	doPrint(format,a)
+}
 
 func PrintInfoF(format string, a ...interface{}){
 	blue.Print("[INFO] ")
-	doPrint(format,a)
+	doPrint(format,a...)
 }
 
 func PrintWarnF(format string, a ...interface{}){
 	yellow.Print("[WARN] ")
-	doPrint(format,a)
+	doPrint(format,a...)
 }
 
 func PrintErrorF(format string, a ...interface{}){
 	red.Print("[ERROR] ")
-	doPrint(format,a)
+	doPrint(format,a...)
 }
 
 func PrintError(err error){
@@ -29,9 +35,9 @@ func PrintError(err error){
 	fmt.Println(err)
 }
 
-func doPrint(format string, a ...interface{}){
+func doPrint(format string, a...interface{}){
 	if len(a) > 0{
-		fmt.Printf(format + "\n",a)
+		fmt.Printf(format + "\n",a...)
 	}else{
 		fmt.Println(format)
 	}
